f72a2c3151990ca2954646fabf1b52b4ea27b816
TusharJain2000 <tusharjain01234@gmail.com>
2024-07-10 19:28:21 +0530

    Created custom react and also deep dived into react source code in github while exploring more react

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -2,13 +2,21 @@ import Chai from "./Components/Chai";
 import Greet from "./Components/Greet";
 import Welcome from "./Components/Welcome";
 function App() {
+  const username = "Tushar@google.com"
   return (
-    <div>
+    <div> 
+      <h1>Learning from Chai aur React {username}</h1>
       <Greet/>
       <Chai/>
       <Welcome/>
     </div>
   )
 }
+
+// Function App or any other function can return only one HTML element.
+// Inside the element there can be any number of elements
+// <></> This is known as fragment. It acts as substitute of div, section, etc.
+// If any new Component is made, it is recommended that the name of the file, function start's in capital letter.
+// Vite, create-react-app, Parcel are bundlers used to import react library 
 
 export default App;
--- 01_vitereact/src/index.jsx
+++ 01_vitereact/src/index.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+// function MyApp(){
+//   return(
+//     <><h1>My Custom Function in index.js</h1></>
+//   )
+// }
+
+// const anotherElement = (
+//   <h1>My custom qwertyuiop</h1>
+// )
+
+
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://www.google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google.com",
+// };
+
+//  const reactElement = React.createElement('a',{href: "https://www.google.com", target: "_blank"}, "click me to visit google")
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App/>
+    
+  </React.StrictMode>
 )
